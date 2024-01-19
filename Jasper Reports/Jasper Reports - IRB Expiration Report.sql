9c00ac1791c9399005d559e5e24bbd5a068fd1bf
Gendyyy <agendyyy@gmail.com>
2024-01-19 10:33:02 -0700

    Revert "Showing protocols with the latest IRB Status, then showing only the ones with expiration date."
    This reverts commit bceced2f6ea10a89b60cb1e64f28038c0deb7778.

--- Jasper Reports/Jasper Reports - IRB Expiration Report.sql
+++ Jasper Reports/Jasper Reports - IRB Expiration Report.sql
@@ -1,7 +1,12 @@
 WITH
     param
     as
-    (SELECT TO_DATE($P{Start_Date}) PSTARTDATE, TO_DATE($P{End_Date}) PENDDATE,  $P{irb}  PIRBC,  $P{MGMT_GROUP} PMGMT_GROUP FROM DUAL)
+    (
+        SELECT TO_DATE($P
+    
+    {Start_Date}) PSTARTDATE, TO_DATE
+( $P{End_Date} ) PENDDATE,  $P{irb}  PIRBC,  $P{MGMT_GROUP} PMGMT_GROUP FROM DUAL)
+, irbDATES AS
 SELECT p.PI_Name piname, p.protocol_no, p.study_target_accrual, p.title, p.total_accrual totalonstudy,
     i.maxeffectivedate, i.maxexpireddate,
     g.sponsor_name, g.sponsor_protocol_no,
@@ -12,14 +17,16 @@ FROM uacc_oncore_rw_utils.protocols p
     LEFT JOIN ( SELECT PROTOCOL_ID, first_name, last_name, FIRST_NAME || ' ' || LAST_NAME  irbcoord_name, stop_date
     FROM uacc_oncore_rw_utils.sv_pcl_staff_ro
     WHERE staff_role = 'Primary IRB Coordinator' AND ACTIVE_FLAG = 'Y' and protocol_subject_id is null ) c ON p.protocol_id = c.protocol_id
-       outer apply (select
+       outer apply (select top 1
         protocol_id, PROTOCOL_NO, ACTION_DATE maxeffectivedate, EXPIRATION_DATE maxexpireddate, review_expires
     from uacc_oncore_prod.rv_pcl_irb_review r
     where 
               r.PROTOCOL_ID = P.PROTOCOL_ID
-              and REVIEW_REASON IN ('Continuing Review','Initial Review')
-              order by EXPIRATION_DATE desc
-              FETCH FIRST 1 ROWS ONLY) I
+              REVIEW_REASON
+IN
+('Continuing Review','Initial Review')
+              order by maxexpireddate desc
+              ) I
   WHERE CURRENT_status NOT IN
 ( 'ABANDONED', 'TERMINATED', 'IRB STUDY CLOSURE', 'NEW','SRC APPROVAL' )
    AND maxexpireddate BETWEEN PSTARTDATE AND PENDDATE
